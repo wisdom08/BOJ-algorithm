2677b0b370587837a4810dac7fc852853aa1f487
wisdom08 <ji4168aw@gmail.com>
2020-09-27 22:41:13 +0900

    Initial commit

---
+++ src/easy/Main01.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class Main01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/easy/Main03.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Main03 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int testNum = sc.nextInt();
+        int [] arr =  new int[testNum];
+
+        for(int i=0; i<testNum; i++){
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            arr[i] = num1 + num2;
+        }
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+
+    }
+}
