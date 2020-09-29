e8a52b4851e47d1c1361787b7cfc4d44a2702928
wisdom08 <ji4168aw@gmail.com>
2020-09-30 01:21:52 +0900

    입출력 문제-11720

---
+++ src/easy/Main11.java
@@ -0,0 +1,26 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testNum = sc.nextInt();
+
+        int[] arr = new int[testNum];
+        int sum = 0;
+
+        String input = sc.next();
+
+        for (int i = 0; i < testNum; i++) {
+            sum += input.charAt(i) -'0';
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
