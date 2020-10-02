4a96258cb5e5cd1012e268c2ca0777fa6ceb9040
wisdom08 <ji4168aw@gmail.com>
2020-10-02 10:54:42 +0900

    입출력 문제-2742

---
+++ src/easy/Main13.java
@@ -0,0 +1,18 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Main13 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int startNum = sc.nextInt();
+
+        for (int i = startNum; i > 0; i--) {
+            System.out.println(i);
+        }
+        sc.close();
+
+    }
+}
