66ada08c4ee7e706fd75c103294080f57af24fc8
wisdom08 <ji4168aw@gmail.com>
2020-10-03 10:07:24 +0900

    입출력 문제

--- src/io/Main28.java
+++ src/io/Main28.java
@@ -5,30 +5,43 @@ import java.util.Scanner;
 //10992
 public class Main28 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        sc.close();
+
+        String leftChar = " ";
+        String starChar = "*";
 
 
-        for(int i=0;i<n;i++) { // 별의 범위에서 양 끝에 속하는 열을 출력해야 하므로, 0부터 시작한다.
-            // 예를들어 n이 4일 경우 첫 번째 행은 4번 열에만 별이 찍히고,
-            // 두 번째 행은 4-1, 4+1 열에만 별이 찍히게 된다.
-            for(int j=1;j<2*n;j++) { // 열의 범위는 별 찍기-16과 동일하다.
-                if(j>n+i) break; // 마지막 별을 찍었으면 반복문을 벗어난다.
-                if(i==n-1) { // 마지막 행에 도착하면 모든 열에 별을 찍는다.
-                    System.out.print("*");
-                    continue;
-                }
-                if(j==n-i || j==n+i) { // 범위의 양 끝에 별을 찍는다.
-                    System.out.print("*");
-                }else
-                    System.out.print(" ");
+        for (int row = 0; row < n; row++) {
+
+            //왼쪽
+            int leftCnt = n-1-row;
+            printChar(leftChar, leftCnt);
+
+            //오른쪽
+            int starCnt = row*2+1;
+
+            //맨 윗줄 1개
+            if(row == 0){
+                printChar(starChar, 1);
+            } else if(row == n-1){
+            //맨 아랫 줄 전부 출력
+            printChar(starChar, starCnt);
+            } else {
+                //양 끝 1개 씩 만 출력
+                printChar(starChar, 1);
+                printChar(leftChar, starCnt-2);
+                printChar(starChar, 1);
             }
             System.out.println();
-
         }
+    }
 
+    private static void printChar(String ch, int cnt) {
+        for (int col = 0; col < cnt; col++) {
+            System.out.print(ch);
+        }
     }
 }
